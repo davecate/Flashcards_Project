70063278faa296db73cb41f367536f3064ae83c7
davecate <86169488+davecate@users.noreply.github.com>
2021-09-24 16:59:16 -0500

    Added comments and organized routes

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -23,27 +23,22 @@ const Layout = () => {
       <Header />
       <div className = "container">
         <Switch>
+          {/* Home screen: a "create deck" button and list of decks 
+          with buttons to view, study, and delete decks */}
           <Route exact path="/">
             <DeckList 
               setDeck={setDeck}
             />
           </Route>
-          <Route path="/decks/:deckId/study">
-            <Study 
-              deck={deck} 
-              setDeck={setDeck} 
-              card={card} 
-              setCard={setCard} 
-              cards={cards} 
-              setCards={setCards}
-            />
-          </Route>
+          {/* Create Deck screen: a form with inputs for a name and description */}
           <Route path="/decks/new">
             <CreateDeck 
               deck={deck} 
               setDeck={setDeck}
             />
           </Route>
+          {/* Deck screen: displays title, description, and list of cards for a given deck
+          with buttons to edit, study, or delete the deck and buttons to add, edit, or view cards */}
           <Route exact path="/decks/:deckId">
             <ViewDeck 
               deck={deck} 
@@ -54,12 +49,25 @@ const Layout = () => {
               setCards={setCards} 
             />
           </Route>
+           {/* Study screen: draw and and flip cards from a given deck */}
+           <Route path="/decks/:deckId/study">
+            <Study 
+              deck={deck} 
+              setDeck={setDeck} 
+              card={card} 
+              setCard={setCard} 
+              cards={cards} 
+              setCards={setCards}
+            />
+          </Route>
+          {/* Edit Deck screen: a form that comes pre-filled with the selected deck's name and description */}
           <Route path="/decks/:deckId/edit">
             <EditDeck 
             deck={deck} 
             setDeck={setDeck} 
             />
           </Route>
+          {/* Add Card screen: a form with inputs for the front and back of the card */}
           <Route path="/decks/:deckId/cards/new">
             <AddCard 
               deck={deck} 
@@ -68,6 +76,7 @@ const Layout = () => {
               setCard={setCard} 
             />
           </Route>
+          {/* Edit Card screen: a form that comes pre-filled with the selected card's front and back text */}
           <Route path="/decks/:deckId/cards/:cardId/edit">
             <EditCard 
               deck={deck} 
@@ -75,6 +84,7 @@ const Layout = () => {
               setCard={setCard} 
             />
           </Route>
+          {/* 404 screen */}
           <Route>
             <NotFound />
           </Route>
